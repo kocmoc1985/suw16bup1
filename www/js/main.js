764e4702f0bb54f9d4afad51e7938353630a8c53
ab <kocmoc1985@gmail.com>
2016-10-26 14:35:12 +0200

    V1.02 - DELL

---
+++ www/js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    initialize();
+});
+
+function initialize() {
+    //
+    includeHtml("content/header.html", "#header");
+    //
+    includeHtml("content/footer.html", "#footer-container-main");
+}
+
+function includeHtml(url, selector, addType) {
+    //
+    var html = $.ajax({
+        url: url,
+        dataType: 'text',
+        async: false
+    }).responseText;
+    //
+    if (addType === "append") {
+        $(selector).append(html);
+    } else if (addType === "prepend") {
+        $(selector).prepend(html);
+    } else if (addType === "after") {
+        $(selector).after(html);
+    } else if (addType === "before") {
+        $(selector).before(html);
+    } else {
+        $(selector).append(html);
+    }
+}
+
+function includeHtmlAsync(url, selector, addType) {
+    $.ajax({
+        url: url,
+        dataType: 'text',
+        async: false
+    }).done(function(msg) {
+        if (addType === "append") {
+            $(selector).append(msg);
+        } else if (addType === "prepend") {
+            $(selector).prepend(msg);
+        } else if (addType === "after") {
+            $(selector).after(msg);
+        } else if (addType === "before") {
+            $(selector).before(msg);
+        } else {
+            $(selector).append(msg);
+        }
+    });
+}
