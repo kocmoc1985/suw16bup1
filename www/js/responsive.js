6bb44d8e31a5ce66d26fdfb420411246602001cc
ab <kocmoc1985@gmail.com>
2016-10-29 18:56:02 +0200

    V1.22 - DELL

--- www/js/responsive.js
+++ www/js/responsive.js
@@ -3,52 +3,6 @@ var MEDIUM = 972; //Bootstrap 992
 var SMALL = 768;
 var X_SMALL = 460; // bootstrap 480
 //
-$(document).ready(function () {
-    //
-    var width = $(window).width();
-    //
-    if (width <= X_SMALL) {
-        $(".blog-entry img").addClass("image-xs-max-width");
-    }
-    //
-    if (width < MEDIUM) {
-        //
-        $(".sidebar-menu").removeClass("hide");
-        $(".sidebar-menu").addClass("show");
-        //
-        $(".sidebar-entry").addClass("sidebar-entry-mobile-view");
-    }
-    //
-    if (width > MEDIUM) {
-        //
-        $(".sidebar-menu").removeClass("show");
-        $(".sidebar-menu").addClass("hide");
-        //
-        $(".sidebar-entry").removeClass("sidebar-entry-mobile-view");
-    }
-    //
-});
-//
-$(window).on('resize orientationChange', function (event) {
-    //
-    var width = $(window).width();
-    //
-    if (width < MEDIUM) {
-        $(".sidebar-menu").removeClass("hide");
-        $(".sidebar-menu").addClass("show");
-        //
-        $(".sidebar-entry").addClass("sidebar-entry-mobile-view");
-    }
-    //
-    if (width > MEDIUM) {
-        $(".sidebar-menu").removeClass("show");
-        $(".sidebar-menu").addClass("hide");
-        //
-        $(".sidebar-entry").removeClass("sidebar-entry-mobile-view");
-    }
-});
-//
-//
 //
 $(window).load(function () {
     //Hide links in mobile mode after clicking on a link
